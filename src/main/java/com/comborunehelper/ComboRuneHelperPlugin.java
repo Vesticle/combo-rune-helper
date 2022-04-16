c52dbc8417990f930a802362f9d93dd9f48e08e8
Vesticle <svest82@protonmail.com>
2022-04-16 07:35:40 -0500

    Testing coords for altars

--- src/main/java/com/comborunehelper/ComboRuneHelperPlugin.java
+++ src/main/java/com/comborunehelper/ComboRuneHelperPlugin.java
@@ -46,37 +46,15 @@ public class ComboRuneHelperPlugin extends Plugin {
 		if (currentLoc == null)
 			return;
 
-		if (currentLoc.distanceTo2D(FIRE_ALTAR) > 20)
+		if (currentLoc.distanceTo2D(FIRE_ALTAR) > 20 && currentLoc.distanceTo2D(WATER_ALTAR) > 30 && currentLoc.distanceTo2D(EARTH_ALTAR) > 30 && currentLoc.distanceTo2D(AIR_ALTAR) < 30){
 			return;
+		} else {
+			MenuEntry[] newEntries = Arrays.stream(client.getMenuEntries())
+					.filter(menuEntry -> !(menuEntry.getTarget().contains("Altar") && menuEntry.getOption().equals("Craft-rune")))
+					.toArray(MenuEntry[]::new);
+			client.setMenuEntries(newEntries);
+		}
 
-		MenuEntry[] newEntries = Arrays.stream(client.getMenuEntries())
-				.filter(menuEntry -> !(menuEntry.getTarget().contains("Altar") && menuEntry.getOption().equals("Craft-rune")))
-				.toArray(MenuEntry[]::new);
-		client.setMenuEntries(newEntries);
-
-		if (currentLoc.distanceTo2D(WATER_ALTAR) > 20)
-			return;
-
-		MenuEntry[] newEntries2 = Arrays.stream(client.getMenuEntries())
-				.filter(menuEntry -> !(menuEntry.getTarget().contains("Altar") && menuEntry.getOption().equals("Craft-rune")))
-				.toArray(MenuEntry[]::new);
-		client.setMenuEntries(newEntries2);
-
-		if (currentLoc.distanceTo2D(EARTH_ALTAR) > 20)
-			return;
-
-		MenuEntry[] newEntries3 = Arrays.stream(client.getMenuEntries())
-				.filter(menuEntry -> !(menuEntry.getTarget().contains("Altar") && menuEntry.getOption().equals("Craft-rune")))
-				.toArray(MenuEntry[]::new);
-		client.setMenuEntries(newEntries3);
-
-		if (currentLoc.distanceTo2D(AIR_ALTAR) > 20)
-			return;
-
-		MenuEntry[] newEntries4 = Arrays.stream(client.getMenuEntries())
-				.filter(menuEntry -> !(menuEntry.getTarget().contains("Altar") && menuEntry.getOption().equals("Craft-rune")))
-				.toArray(MenuEntry[]::new);
-		client.setMenuEntries(newEntries4);
 	}
 
 }
