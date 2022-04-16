85ef4367e607d8769c2f782d540bbdadd5517e43
Vesticle <svest82@protonmail.com>
2022-04-16 06:56:51 -0500

    Testing coords for altars

--- src/main/java/com/comborunehelper/ComboRuneHelperPlugin.java
+++ src/main/java/com/comborunehelper/ComboRuneHelperPlugin.java
@@ -1,53 +1,64 @@
 package com.comborunehelper;
 
-import com.google.inject.Provides;
-import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.client.config.ConfigManager;
+import net.runelite.api.MenuEntry;
+import net.runelite.api.Player;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.events.ClientTick;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
+import javax.inject.Inject;
+import java.util.Arrays;
+
 @Slf4j
 @PluginDescriptor(
-	name = "Combo Rune Helper"
+		name = "ComboRuneHelper"
 )
-public class ComboRuneHelperPlugin extends Plugin
-{
+public class ComboRuneHelperPlugin extends Plugin {
 	@Inject
 	private Client client;
 
-	@Inject
-	private ComboRuneHelperConfig config;
+	private static final WorldPoint FIRE_ALTAR = new WorldPoint(2583, 4839, 0);
+	private static final WorldPoint WATER_ALTAR = new WorldPoint(2723, 4839, 0);
+	private static final WorldPoint EARTH_ALTAR = new WorldPoint(2863, 4839, 0);
+	private static final WorldPoint AIR_ALTAR = new WorldPoint(3083, 4839, 0);
 
 	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Example started!");
+	protected void startUp() throws Exception {
+		log.info("ComboRunesOnly started!");
 	}
 
 	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Example stopped!");
+	protected void shutDown() throws Exception {
+		log.info("ComboRunesOnly stopped!");
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
+	public void onClientTick(ClientTick event) {
+		Player p = client.getLocalPlayer();
+		if (p == null)
+			return;
+
+		WorldPoint currentLoc = p.getWorldLocation();
+		if (currentLoc == null)
+			return;
+
+		if (currentLoc.distanceTo2D(FIRE_ALTAR) > 20)
+			return;
+		if (currentLoc.distanceTo2D(WATER_ALTAR) > 20)
+			return;
+		if (currentLoc.distanceTo2D(EARTH_ALTAR) > 20)
+			return;
+		if (currentLoc.distanceTo2D(AIR_ALTAR) > 20)
+			return;
+
+		MenuEntry[] newEntries = Arrays.stream(client.getMenuEntries())
+				.filter(menuEntry -> !(menuEntry.getTarget().contains("Altar") && menuEntry.getOption().equals("Craft-rune")))
+				.toArray(MenuEntry[]::new);
+		client.setMenuEntries(newEntries);
 	}
 
-	@Provides
-	ComboRuneHelperConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(ComboRuneHelperConfig.class);
-	}
 }
